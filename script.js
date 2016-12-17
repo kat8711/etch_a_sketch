9c2be50f33d974886047a1ea4e0e0693bc26f716
Kathryn Rose <krose8711@gmail.com>
2016-12-17 17:40:50 -0600

    Added feature: cell colors change on mouseover

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ console.log("Hello!");
 $(document).ready(function()
 {
     var tableSize = 16;
+    var position = 0;
+    var id;
+    var cell;
+    var color = "rainbow";
 
     $("#grid").append("<table>")
 
@@ -12,15 +16,41 @@ $(document).ready(function()
 
        for (j = 1; j <= tableSize; j++)
        {
-        $("#grid").append("<td></td>");
+        $("#grid").append("<td id =" + position + "></td>");
+        position++;
        }
 
        $("#grid").append("</tr>")
+
     }
 
     $("#grid").append("</table>");
 
     console.log(randomColor());
+
+    $('button#black').click(function() {
+        color = "black";
+    })
+
+    $('button#rainbow').click(function() {
+        color = "rainbow";
+    })
+
+    $('td').mouseenter(function() {
+        id = $(this).attr('id');
+        cell = "td#" + id;
+        if (color === "rainbow")
+        {
+        $(cell).css('background-color',randomColor());
+        }
+        else if (color === "black") {
+        $(cell).css('background-color','#000');
+        }
+    })
+
+    $('button#reset').click(function() {
+        $('td').css('background-color','#ddd');
+    })
 });
 
 var randomColor = function()
@@ -28,6 +58,7 @@ var randomColor = function()
     var hexCode = "#";
 
     for (i = 1; i <= 6; i++) {
+        var digit;
         var num = Math.floor(Math.random() * 17);
 
         if (num > 9)
